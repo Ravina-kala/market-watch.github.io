71cd6899ad4dc245789e79c1d2275ad6afbf4db5
Ravina-kala <ravinakala81@gmail.com>
2024-09-18 16:36:34 +0530

    Update script.js trying another method to autosize y axis

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-let data = {};
-
-// Function to update message on the screen
-function displayMessage(message) {
-    const messageDiv = document.getElementById("message");
-    messageDiv.innerHTML = message;
-}
-
-// Function to load and merge multiple JSON files
-function loadData() {
-    const jsonFiles = ['data_1d_part_22.json', 'data_1d_part_9.json', 'data_1d_part_34.json', 'data_1d_part_40.json', 'data_1d_part_26.json', 'data_1d_part_27.json', 'data_1d_part_20.json', 'data_1d_part_6.json', 'data_1d_part_2.json', 'data_1d_part_32.json', 'data_1d_part_7.json', 'data_1d_part_24.json', 'data_1d_part_1.json', 'data_1d_part_16.json', 'data_1d_part_35.json', 'data_1d_part_31.json', 'data_1d_part_38.json', 'data_1d_part_11.json', 'data_1d_part_37.json', 'data_1d_part_39.json', 'data_1d_part_36.json', 'data_1d_part_12.json', 'data_1d_part_33.json', 'data_1d_part_10.json', 'data_1d_part_4.json', 'data_1d_part_30.json', 'data_1d_part_23.json', 'data_1d_part_21.json', 'data_1d_part_5.json', 'data_1d_part_17.json', 'data_1d_part_29.json', 'data_1d_part_13.json', 'data_1d_part_14.json', 'data_1d_part_15.json', 'data_1d_part_19.json', 'data_1d_part_18.json', 'data_1d_part_28.json', 'data_1d_part_8.json', 'data_1d_part_25.json', 'data_1d_part_3.json'];
-
-    const fetchPromises = jsonFiles.map(file => fetch(file).then(response => response.json()));
-
-    Promise.all(fetchPromises)
-        .then(jsonParts => {
-            // Merge all parts into the 'data' object
-            jsonParts.forEach(part => {
-                Object.assign(data, part);
-            });
-            displayMessage("All data successfully loaded from JSON files.");
-        })
-        .catch(error => displayMessage('Error loading JSON files: ' + error));
-}
-
-// Call loadData when the page loads
-window.onload = loadData;
-
 function plotGraph() {
     const tick = document.getElementById("search-box").value.trim();
 
@@ -135,7 +107,7 @@ function plotGraph() {
             },
             xaxis: {
                 type: 'category',
-                rangeslider: { visible: true },  // Disable the date slider
+                rangeslider: { visible: true },  // Enable the date slider
                 range: [dates[dates.length - 50], dates[dates.length - 1]],  // Initial fixed range (last 50 dates)
                 showticklabels: false,  // Remove date labels on the x-axis
             },
@@ -160,6 +132,34 @@ function plotGraph() {
 
         // Plot the chart
         Plotly.newPlot('plot', [candlestick, k_trace, d_trace, volume_trace, buy_signal_trace, sell_signal_trace], layout);
+
+        var myPlot = document.getElementById('plot');
+
+        var isUnderRelayout = false;
+        myPlot.on('plotly_relayout', function(relayoutData) {
+            if (!isUnderRelayout) {
+                isUnderRelayout = true;
+
+                // Get the start and end dates of current 'view'
+                var start = relayoutData['xaxis.range'][0];
+                var end = relayoutData['xaxis.range'][1];
+
+                // Get the index of the start and end dates
+                var xstart = myPlot.data[0].x.map(e => e).indexOf(start.substring(0, 10));
+                var xend = myPlot.data[0].x.map(e => e).indexOf(end.substring(0, 10));
+
+                if (xstart < 0) { xstart = 0; } // sometimes the date is before the data and returns -1
+
+                // Get the min and max's
+                var low = Math.min.apply(null, myPlot.data[0].low.slice(xstart, xend));
+                var high = Math.max.apply(null, myPlot.data[0].high.slice(xstart, xend));
+
+                // Update the yaxis range and set flag to false
+                var update = { 'yaxis1.range': [low, high] };
+                Plotly.relayout(myPlot, update).then(() => { isUnderRelayout = false });
+            }
+        });
+
         displayMessage(`Plot created successfully for ${tick}.`);
     } else {
         displayMessage(`No data found for tick: ${tick}`);
